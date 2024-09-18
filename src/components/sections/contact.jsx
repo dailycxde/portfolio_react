9c9a38d7001e5de8feddd58a6f285cdae15cf3c6
HasanShkoukani <hasanshkoukani19@gmail.com>
2024-09-18 17:07:31 +0400

    first commit

--- src/components/sections/contact.jsx
+++ src/components/sections/contact.jsx
@@ -15,7 +15,7 @@ function Contact() {
                     <div className="f2">
                         <textarea name="message" id="message" rows="3" placeholder="type your message here" required />
                     </div>
-                    <input id="submit" type="submit" required />
+                    <button id="submit" type="submit" required />
                 </form>
             </div>
         </section>
