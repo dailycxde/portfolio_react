214b06033f06a4bdf5726f6719095bb35aa2151c
HasanShkoukani <hasanshkoukani19@gmail.com>
2024-09-18 17:11:27 +0400

    first commit

--- src/components/sections/contact.jsx
+++ src/components/sections/contact.jsx
@@ -9,8 +9,8 @@ function Contact() {
             <div className="window">
                 <form className="form" netlify>
                     <div className="f1">
-                        <input id="name" type="name" placeholder="john smith" />
-                        <input id="email" type="email" placeholder="johnsmith@gmail.com" required />
+                        <input id="name" type="name" name="name" placeholder="john smith" />
+                        <input id="email" type="email" name="email" placeholder="johnsmith@gmail.com" required />
                     </div>
                     <div className="f2">
                         <textarea name="message" id="message" rows="3" placeholder="type your message here" required />
